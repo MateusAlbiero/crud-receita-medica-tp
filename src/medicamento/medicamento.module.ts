31bb8d34d608fb9bdb4c068258f4750145282735
Mateus Albiero <mateusalbiero1@gmail.com>
2024-07-10 17:17:04 -0300

    :sparkles:feat: Adicionado modulo

--- src/medicamento/medicamento.module.ts
+++ src/medicamento/medicamento.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MedicamentoService } from './medicamento.service';
 import { MedicamentoController } from './medicamento.controller';
+import { Medicamento } from './entities/medicamento.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Medicamento])],
   providers: [MedicamentoService],
   controllers: [MedicamentoController]
 })
