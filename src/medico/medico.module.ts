684457957142bdb6edc917dc57e627990ee4d453
Mateus Albiero <mateusalbiero1@gmail.com>
2024-07-10 16:49:21 -0300

    :sparkles:feat: Incluído os arquivos entity

--- src/medico/medico.module.ts
+++ src/medico/medico.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Medico } from './medico.entity';
 import { MedicoService } from './medico.service';
 import { MedicoController } from './medico.controller';
+import { Medico } from './entities/medico.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Medico])],
   providers: [MedicoService],
-  controllers: [MedicoController],
+  controllers: [MedicoController]
 })
 export class MedicoModule {}
--- src/paciente/paciente.module.ts
+++ src/paciente/paciente.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PacienteService } from './paciente.service';
 import { PacienteController } from './paciente.controller';
+import { Paciente } from './entities/paciente.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Paciente])],
   providers: [PacienteService],
   controllers: [PacienteController]
 })
