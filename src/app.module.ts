c2a723ca4222d446e89d5fdcb9ef96f052a40987
Mateus Albiero <mateusalbiero1@gmail.com>
2024-07-10 16:50:50 -0300

    :construction:wip: ajustes gerais projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PacienteModule } from './paciente/paciente.module';
 import { MedicamentoModule } from './medicamento/medicamento.module';
 import { MedicoModule } from './medico/medico.module';
 import { ReceitaModule } from './receita/receita.module';
 
 @Module({
-  imports: [PacienteModule, MedicamentoModule, MedicoModule, ReceitaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: '',
+      password: '',
+      database: '',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    PacienteModule,
+    MedicamentoModule,
+    MedicoModule,
+    ReceitaModule,
+  ],
 })
 export class AppModule {}
